20bc541f1f66cbf657dcf8bd2c9e426985c9ceb4
Travers Dunkinson <tjdunkinson@gmail.com>
2015-12-13 00:11:35 +1100

    Built core prototype gameplay
    jubugu

---
+++ Unity/LD34/Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+
+	float yOffset;
+	float zOffset;
+	Vector3 trans;
+
+	// Use this for initialization
+	void Start () {
+
+	//	yOffset = transform.position.y - (player1.transform.position.y);
+		zOffset = transform.position.z;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		trans.y = (player1.transform.position.y); //- player2.transform.position.y) / 2;
+		//trans.y = trans.y - yOffset;
+		trans.z = zOffset;
+		transform.position = trans;
+
+	
+	}
+}
---
+++ Unity/LD34/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int playerNum;
+	public float baseSpeed;
+	public float accel;
+	public float topSpeed;
+	public float friction;
+
+	public Vector2 velocity;
+	private Rigidbody2D rigid;
+	bool leftTom = false;
+	bool rightTom = false;
+
+	// Use this for initialization
+	void Start () {
+
+		rigid = GetComponent<Rigidbody2D> ();
+
+		velocity = new Vector2 (0, baseSpeed);
+		rigid.velocity = velocity;
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ApplyAccel ();
+
+
+
+		rigid.velocity = velocity;
+	}
+
+	void ApplyAccel ()
+	{
+		if (Input.GetButtonDown ("Player" + playerNum + "RightTom")) 
+		{
+			rightTom = true;
+		}
+
+		if (Input.GetButtonDown ("Player" + playerNum + "LeftTom")) 
+		{
+			leftTom = true;
+		}
+
+		if (leftTom == true && rightTom == true)
+		{
+			velocity += new Vector2(0,accel);
+			leftTom = false;
+			rightTom = false;
+		}
+
+		velocity.y -= friction / (topSpeed - velocity.y);
+		print (friction / (topSpeed - velocity.y));
+		if (velocity.y > topSpeed)
+			velocity = new Vector2 (0, topSpeed);
+
+		if (velocity.y < baseSpeed)
+			velocity = new Vector2 (0, baseSpeed);
+	}
+}
